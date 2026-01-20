cd50a77a65bbca12e7a7e284a11cff627ba7ca50
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2026-01-20 17:36:47 -0300

    Update Projects

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ProjectLink = ({ repo }) => (
     <a href={repo} target="_blank" rel="noopener noreferrer" className="project-link-text">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" style={{ marginRight: "5px", width: '20px', height: '20px' }} />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" style={{ marginRight: "5px", width: '20px', height: '20px', color: 'red' }} />
         <span>GitHub</span>
         <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" style={{ width: '20px', height: '20px' }} />
     </a>
